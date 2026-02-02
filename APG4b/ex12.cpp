24de064f5391a56eabb90deb49baf6ba8f8f7456
Suzu Yasu <suzukiyasu1116@gmail.com>
2026-02-03 07:36:44 +0900

    Create ex12.cpp

---
+++ APG4b/ex12.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string S;
+  cin >> S;
+  int ans = 1;
+  for(int i = 1; i < S.size() ; i++){
+  if(S.at(i) == '+'){
+    ans += 1;
+  }
+  else if(S.at(i) == '-'){
+    ans -= 1;
+  }
+  else{
+    ans += 0;
+  }
+  }
+  cout << ans << endl;
+}
