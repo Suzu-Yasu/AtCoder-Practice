3d68c57f28484ff66bda4835038864d5eceecbf7
Suzu Yasu <suzukiyasu1116@gmail.com>
2026-02-02 23:52:23 +0900

    Create ex10.cpp

---
+++ APG4b/ex10.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int A, B;
+  cin >> A >> B;
+  
+  cout <<"A:";
+  int i = 0;
+  while(i < A){ 
+    cout << "]";
+    i++;}
+  
+  
+  cout << endl << "B:";
+  int j = 0;
+  while(j < B){ 
+    cout << "]";
+    j++;}
+  cout << endl;}
